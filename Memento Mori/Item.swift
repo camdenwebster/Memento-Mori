84fdab142449299f53dafa0c4a962516f027ffef
Camden Webster <59626059+camdenwebster@users.noreply.github.com>
2024-12-09 07:03:16 -0600

    Initial Commit

---
+++ Memento Mori/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Memento Mori
+//
+//  Created by Camden Webster on 12/9/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Memento MoriTests/Memento_MoriTests.swift
@@ -0,0 +1,17 @@
+//
+//  Memento_MoriTests.swift
+//  Memento MoriTests
+//
+//  Created by Camden Webster on 12/9/24.
+//
+
+import Testing
+@testable import Memento_Mori
+
+struct Memento_MoriTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
